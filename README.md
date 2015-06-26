1be6f1e13d921e7bfa0d0888b979680ef65b9301
antirek <serge.dmitriev@gmail.com>
2015-06-26 11:39:10 +0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lua-dialplan
 asterisk dialplan in extensions.lua 
+
+
+## Prepare
+
+1. install lua
+
+>apt-get install lua5.2 liblua5.2-dev
+
+2. build asterisk with lua
+
+
+
---
+++ main.lua
@@ -0,0 +1,56 @@
+
+function inner()
+	return {
+		["1200"] = inner_call("1200");
+		["1300"] = inner_call("1300");
+		["1400"] = inner_call("1400");
+	}
+end;
+
+
+local conf = {
+	["1200"] = "SIP/1234";
+	["1300"] = "SIP/2222";
+};
+
+
+function inner2(t)
+	local v = {}
+	for key, value in pairs(t) do
+    	v[key] = inner_call(value);
+	end;
+	return v
+end;
+
+
+function inner_call(e)
+	return function ()
+		app.playback('beep')
+		app.noop(e)
+		app.hangup()
+	end;
+end;
+
+
+local D = {
+	extensions = {
+		["maga"] = inner2(conf);
+	};
+
+	hints = {
+
+	};
+};
+
+
+
+
+Dialplan = {
+	getExtensions = function ()
+		return D.extensions
+	end;
+
+	getHints = function ()
+		return D.hints
+	end;
+};
