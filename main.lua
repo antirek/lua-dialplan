d60b8b78c34c846839365cdd1540d0e592e0dd7c
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-10-05 17:26:09 +0700

    add using mongodb

--- main.lua
+++ main.lua
@@ -3,33 +3,56 @@ local mongo = require 'mongo'
 local db = mongo.Connection.New()
 db:connect('localhost')
 
+local inspect = require 'inspect'
 
 file = io.open("/tmp/test.lua.log", "a+")
 io.output(file)
 
-function inner_call (context, extension)
-	-- local c1 = db:count('test.values')
-	-- io.write('count')
-	-- io.write(c1)
-	if (extension:len() == 4) then 
-		app["goto"]('outbound', extension, 1);  --its goto to native dialplan of asterisk
-	end;
 
-	local peername = channel.CHANNEL("peername"):get()
+function inner_call (context, extension)
+	
+	peername = channel.CHANNEL("peername"):get()
 	name = channel.CALLERID("name"):get()
 	num = channel.CALLERID("num"):get()
 	all = channel.CALLERID("all"):get()
+	app.noop("extension "..extension)
+
+	local q1 = db:query("test.values", {peername = peername}):results();
+	local settings = {
+		["peer"] = q1{1};
+	};
+
+	app.noop("peer:"..settings.peer.vpbxId)
 	
+	local q2 = db:query("test.values", { vpbxId = settings.peer.vpbxId, peername = extension}):results();
+	settings["peers"] = q2
+	local target = settings.peers{1}.peername
+	app.noop('target'..target)
+	
+
+	if (target ~= '') then
+		--app.playback('beep')
+		app.dial('SIP/'..target)
+	else 
+		app.hangup()
+	end;
+
+--[[ 
 	app.noop("channel = "..peername)
 	app.noop("callerid name = "..name)
 	app.noop("callerid num = "..num)
 	app.noop("callerid all = "..all)
+	app.noop("callerid vpbxId = "..settings.peer.vpbxId)
+	app.noop("callerid vpbx size = "..settings.peers)
 	
 	app.playback('beep')
 	app.dial('SIP/'..extension)
 	app.noop(context)
 	app.noop(extension)
 	app.hangup()
+
+	]]
+
 end;
 
 
@@ -44,7 +67,7 @@ local D = {
 		};
 
 		["outbound2"] = {
-			['_XXXX'] = inner_call;
+			['_XXXX'] = inner_call
 		};
 	};
 
