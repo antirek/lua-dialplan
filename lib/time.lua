a3a29b92e758cf4a86ca14ec4c3eeefa283c71ec
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-11-03 17:49:19 +0700

    add time day comparison

--- lib/time.lua
+++ lib/time.lua
@@ -1,7 +1,18 @@
+local inspect = require('inspect');
+
 return function (dbHelper)
 
     function time (context, extension)
-        local time = dbHelper.findTimeByExtension(extension);        
+        local date = os.date("*t");
+        app.noop('day: '..date["day"]);
+        local time = dbHelper.findTimeByExtension(extension);
+        app.noop('baserules: '..inspect(time.baserules));
+        for i, rule in ipairs(time.baserules) do 
+            if (rule.day == date["wday"]) then 
+                app["goto"](rule.target);
+            end; 
+        end;
+
         app["goto"](time.default);
     end;
 
